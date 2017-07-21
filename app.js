3d570b9b2928129b782e0c534742b4911d1da518
Ram Sharma <Ram Sharma>
2017-07-21 21:38:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var http=require('http');
+var express= require('express');
+var path = require('path');
+
+var app = express();
+var port = process.env.port || 1337;
+app.use(express.static(path.join(__dirname, 'www')));
+
+app.get('/', function(req, res){
+
+    res.sendFile(path.join(__dirname + '/www/Index.html'));
+
+});
+
+http.createServer(app).listen(port, function () {
+	console.log('Server is running. Point your browser to: http://localhost:' + port);
+});
