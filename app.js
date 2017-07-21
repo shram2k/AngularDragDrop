17b61055fa750a5d7011cb2dd5a521d434dcd3f5
Ram Sharma <Ram Sharma>
2017-07-21 21:57:08 +0530

    second

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var http=require('http');
 var express= require('express');
 var path = require('path');
-
 var app = express();
 var port = process.env.port || 1337;
 app.use(express.static(path.join(__dirname, 'www')));
@@ -14,4 +13,4 @@ app.get('/', function(req, res){
 
 http.createServer(app).listen(port, function () {
 	console.log('Server is running. Point your browser to: http://localhost:' + port);
-});
+});// open web server
